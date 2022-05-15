6c9676b51bf37e07d35f139102e0d9ee7e12bbfa
Sepsi László <48379676+SepsiLaszlo@users.noreply.github.com>
2022-05-15 23:06:54 +0200

    add comment send

--- src/components/@pages/movie/DetailPage.tsx
+++ src/components/@pages/movie/DetailPage.tsx
@@ -1,17 +1,39 @@
 import {
+  Box,
+  Button,
+  FormControl,
+  FormLabel,
+  HStack,
+  Input,
   StackDivider,
   Text,
+  useToast,
   VStack,
 } from "@chakra-ui/react";
 import { FC, useEffect, useState } from "react";
 import { Page } from "../../Page";
 import axios from "axios";
-import { MovieWithComments } from "../../Dtos";
+import { MovieWithComments, User } from "../../Dtos";
 import { useParams } from "react-router-dom";
-import { baseURL } from "../../constants";
+import { baseURL, basicTheme } from "../../constants";
 import { MovieComponent } from "../../MovieComponent";
 export const MovieDetailPage: FC = (props) => {
   let params = useParams();
+
+  useEffect(() => {
+    if (user) {
+      return;
+    }
+    const userId = window.localStorage.getItem("userId");
+    if (userId === null) {
+      return;
+    }
+
+    axios.get(baseURL + "/User/" + userId).then((response) => {
+      setUser(response.data);
+    });
+  });
+  const [user, setUser] = useState<User>();
 
   const [movie, setMovies] = useState<MovieWithComments>();
   useEffect(() => {
@@ -20,6 +42,45 @@ export const MovieDetailPage: FC = (props) => {
       setMovies(response.data);
     });
   }, []);
+  const toast = useToast();
+
+  function sendComment() {
+    console.log("Send comment...");
+    const comment = (document.getElementById("comment") as HTMLInputElement)
+      ?.value;
+
+    axios
+      .post(baseURL + "/comments", {
+        text: comment,
+        userId: user?.id,
+        movieId: movie?.id,
+      })
+      .then((response: any) => {
+        let updatedComments = movie?.comments;
+        if (updatedComments && user && movie) {
+          updatedComments.push({
+            id: 1,
+            text: comment,
+            user: {
+              id: user.id,
+              email: user.email,
+              name: user.name,
+              password: user.password,
+            },
+          });
+          movie.comments=updatedComments
+          setMovies(movie)
+        }
+
+        toast({
+          title: "Komment elküldve!",
+          description: "A kommentedet sikeresen elmentettük!",
+          status: "success",
+          duration: 9000,
+          isClosable: true,
+        });
+      });
+  }
 
   return (
     <Page>
@@ -32,7 +93,7 @@ export const MovieDetailPage: FC = (props) => {
       )}
 
       <Text fontSize="xl" mb={5}>
-        Kommnetek:
+        Kommentek:
       </Text>
       {(!movie || (movie && movie.comments.length == 0)) && (
         <Text fontSize="lg">Nincsenek kommentek</Text>
@@ -53,6 +114,27 @@ export const MovieDetailPage: FC = (props) => {
             <Text>{comment.text}</Text>
           </VStack>
         ))}
+
+      {!user && <Text>Kommentek írásához jelentkezz be!</Text>}
+      {user && (
+        <Box maxWidth="1000px" marginX="auto">
+          <HStack>
+            <Text>Komment</Text>
+          </HStack>
+          <HStack borderColor="primary" flexGrow={1}>
+            <Input id="comment" type="text" borderWidth={3} flexGrow={1} />
+            <Button
+              flexGrow={1}
+              px={10}
+              maxW="500px"
+              {...basicTheme}
+              onClick={sendComment}
+            >
+              Komment beküldése
+            </Button>
+          </HStack>
+        </Box>
+      )}
     </Page>
   );
 };
